bdd3d0f5bde0e0cc85feaef7cc601827b1c26834
John Mutua <ymutua@gmail.com>
2024-10-06 16:21:14 +0100

    data download section

--- src/2Feed-geoprocessing/2PrepareTreeCover_clip.R
+++ src/2Feed-geoprocessing/2PrepareTreeCover_clip.R
@@ -4,18 +4,18 @@ library(stars)
 library(sf)
 library(exactextractr)
 
-#clip
+# root folder
+root <- "/home/s2255815/rdrive/AU_IBAR/ruminant-feed-balance"
+proPath <- paste0(root, "/src/1Data-download/SpatialData/inputs/TreeCover")
+outdir <- paste0(root, "/src/2Feed-geoprocessing/SpatialData/inputs/TreeCover"); dir.create(outdir, F, T)
 
-setwd("")
-
-proPath <- 'SpatialData/inputs/TreeCover/'
+# read AOI
+aoi_path <- paste0(root, "/src/2Feed-geoprocessing/SpatialData/inputs/AdminBound/aoi0.shp")
 
 filenamesTifInter <- list.files(path = proPath ,pattern="*.tif$",full.names = T)
-filenamesTifInter2 <- list.files(path = proPath ,pattern="*.tif$",full.names = F)
-
+#filenamesTifInter2 <- list.files(path = proPath ,pattern="*.tif$",full.names = F)
 
 ##@Resample and crop with gdal?
 for(i in 1:length(filenamesTifInter)){
-  gdalwarp(srcfile = filenamesTifInter[i], dstfile = paste0(proPath, filenamesTifInter2[i]), overwrite = T, tr = c(0.00297619, 0.00297619), r = "bilinear", cutline = "/exports/eddie/scratch/sfraval/feed-surfaces/SpatialData/inputs/aoi1.shp", crop_to_cutline = T) #0.00297619, 0.00297619
-}  
-
+  gdalwarp(srcfile = filenamesTifInter[i], dstfile = paste0(outdir, "/", basename(filenamesTifInter)), overwrite = T, tr = c(0.00297619, 0.00297619), r = "bilinear", cutline = aoi_path, crop_to_cutline = T) #0.00297619, 0.00297619
+} 
