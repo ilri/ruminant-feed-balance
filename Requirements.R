e90b8e7fd4d8513b8f39db4bf78625b71fd603de
John Mutua <ymutua@gmail.com>
2024-12-11 20:19:07 +0000

    changed zonation

---
+++ Requirements.R
@@ -0,0 +1,9 @@
+
+
+#R version passing
+#R version 4.4.x
+
+#R packages
+install.packages("devtools", repos="http://cran.us.r-project.org")
+install.packages(c("devtools", "tidyverse", "rvest", "ncdf4", "rgdal", "gdalUtils", "flexsurv", "terra", "raster", "stars", "sf", "exactextractr", "rnaturalearth", "gridExtra", "ggtext", "ggsci", "extrafont", "geodata", "RCurl", "httr", "googledrive", "tidpyverse", "FAOSTAT"), repos="http://cran.us.r-project.org") 
+
